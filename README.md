91cbd686f3d875801d4cb90a1a0a54b44eab7a7e
Luca Agnello <luca@gnello.com>
2021-04-27 00:19:44 +0200

    clean up

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-TODO: non usare memcpy per le strutture dati, chi le utilizza si preoccupa di copiare il dato
+# SO project
--- lib/sc_handler/sc_handler.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef PROJECT_SC_HANDLER_H
-#define PROJECT_SC_HANDLER_H
-
-void* sc_malloc_h(size_t size);
-
-#endif //PROJECT_SC_HANDLER_H
