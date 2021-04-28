1ebf147b6914d63bccc4b96735d16cc235d98a11
Luca Agnello <luca@gnello.com>
2021-04-28 23:31:40 +0200

    Improve makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,17 @@
-TARGETS = helpers data-structures
+CC = gcc
+CFLAGS += -std=c99 -Wall -pedantic -g
+LIBS += -Wl,-rpath,./data-structures/lib -L./data-structures/lib -lgnl_ts_queue_t
+INCLUDES += -I./data-structures/includes
+TARGETS = main
 
 .PHONY: all clean helpers data-structures
 
+VPATH = src
+
 all: $(TARGETS)
+
+%: %.c data-structures
+	$(CC) $(CFLAGS) $(INCLUDES) $(OPTFLAGS) -o $@ $< $(LDFLAGS) $(LIBS)
 
 helpers:
 	cd ./helpers && $(MAKE)
@@ -11,6 +20,7 @@ data-structures:
 	cd ./data-structures && $(MAKE)
 
 clean:
+	rm -f $(TARGETS)
 	cd ./helpers && $(MAKE) clean
 	cd ./data-structures && $(MAKE) clean
 
--- src/main.c
+++ src/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
-#include <sc_handler.h>
+#include <stdlib.h>
+#include <gnl_ts_queue_t.h>
 
 int main() {
-    printf("Hello, World!\n");
+    gnl_ts_queue_t *queue;
+
+    queue = gnl_ts_queue_init(GNL_TS_QUEUE_FIFO);
+    //queue = gnl_ts_queue_init(GNL_TS_QUEUE_LIFO);
+
+    if (queue == NULL) {
+        perror("gnl_ts_queue_init");
+
+        exit(EXIT_FAILURE);
+    }
+
+    char *a = "Hello";
+    char *b = "World";
+    char *c = "!";
+    gnl_ts_queue_push(&queue, a);
+    gnl_ts_queue_push(&queue, b);
+    gnl_ts_queue_push(&queue, c);
+
+    printf("%s", (char *)gnl_ts_queue_pop(&queue));
+    printf("%s", (char *)gnl_ts_queue_pop(&queue));
+    printf("%s", (char *)gnl_ts_queue_pop(&queue));
+    printf("\n");
+
+    gnl_ts_queue_destroy(&queue);
+
     return 0;
 }
-//gcc -Wall -g src/main.c -o myprog -L lib/* -I lib/* -lsc_handler
