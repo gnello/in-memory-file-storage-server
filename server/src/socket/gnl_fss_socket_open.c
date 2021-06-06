e6a1387bfb08dcd86171b194d7367819c4b0e2b6
Luca Agnello <luca@gnello.com>
2021-06-07 00:35:41 +0200

    Add gnl_fss_socket_open

--- server/src/socket/gnl_fss_socket_open.c
+++ server/src/socket/gnl_fss_socket_open.c
@@ -1,12 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include "../../include/socket/gnl_fss_socket_open.h"
 
-const int GNL_FSS_SOCKET_OPEN = 1;
-
-struct gnl_fss_socket_open {
-    const char *pathname;
-    int flags;
-};
-
-int gnl_fss_socket_open_size(struct gnl_fss_socket_open open) {
-    return strlen(open.pathname) + sizeof(open.flags);
+#define GNL_ALLOCATE_MESSAGE(ptr, len) {        \
+    ptr = (char *)malloc(len * sizeof(char));   \
+    if (ptr == NULL) {                          \
+        errno = ENOMEM;                         \
+                                                \
+        return -1;                              \
+    }                                           \
 }
+
+/**
+ * Calculate the size of the open message.
+ *
+ * @param open  The open message.
+ *
+ * @return      The size of the open message.
+ */
+static int message_size(const struct gnl_fss_socket_open open) {
+    return
+    sizeof(int) // the operation id
+    + sizeof(size_t) // the length of the pathname
+    + strlen(open.pathname) // the pathname
+    + sizeof(open.flags); // the flags
+}
+
+int gnl_fss_socket_open_prepare_message(const struct gnl_fss_socket_open message, char **dest) {
+    GNL_ALLOCATE_MESSAGE(*dest, message_size(message) + 1)
+
+    sprintf(*dest, "%d%lu%s%d", GNL_FSS_SOCKET_OPEN, strlen(message.pathname), message.pathname, message.flags);
+
+    return 0;
+}
+
+#undef GNL_ALLOCATE_MESSAGE
