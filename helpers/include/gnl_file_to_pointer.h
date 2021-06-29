951d41c5f1e80cd619522e3ed7b1c6ab985e2000
Luca Agnello <luca@gnello.com>
2021-06-29 22:29:32 +0200

    Add gnl_file_to_pointer

---
+++ helpers/include/gnl_file_to_pointer.h
@@ -0,0 +1,16 @@
+
+#ifndef GNL_FILE_TO_POINTER_H
+#define GNL_FILE_TO_POINTER_H
+
+/**
+ * Put the content of the file located at the given filepath in the given pointer.
+ *
+ * @param filepath  The path of the file to read.
+ * @param pointer   The pointer where to put the file read.
+ * @param size      The pointer where to store the size of the file read.
+ *
+ * @return          Returns 0 on success, -1 otherwise.
+ */
+extern int gnl_file_to_pointer(char *filepath, char **pointer, long *size);
+
+#endif //GNL_FILE_TO_POINTER_H
---
+++ helpers/src/gnl_file_to_pointer.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int gnl_file_to_pointer(char *filepath, char **pointer, long *size) {
+    int res;
+
+    // open the file
+    FILE* fp = fopen(filepath, "rb");
+    if (fp == NULL) {
+        return -1;
+    }
+
+    // seek at the end to get the size of the file
+    res = fseek(fp, 0L, SEEK_END);
+    if (res == -1) {
+        fclose(fp);
+
+        return -1;
+    }
+
+    // get the size of the file and seek at the beginning of it
+    *size = ftell(fp);
+    rewind(fp);
+
+    // allocate memory
+     *pointer = calloc(*size + 1, sizeof(char));
+    if (*pointer == NULL) {
+        fclose(fp);
+        errno = ENOMEM;
+
+        return -1;
+    }
+
+    // read the file
+    if (fread(*pointer, sizeof(char), *size, fp) <= 0) {
+        int errno_cpy = errno;
+
+        if (ferror(fp)) {
+            fclose(fp);
+            free(*pointer);
+            errno = errno_cpy;
+
+            return -1;
+        }
+    }
+
+    res = fclose(fp);
+    if (res == EOF) {
+        return -1;
+    }
+
+    return 0;
+}
