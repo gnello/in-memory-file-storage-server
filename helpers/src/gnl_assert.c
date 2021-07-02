d6ff09c81fe9e8848f88beb70527478f40d39176
Luca Agnello <luca@gnello.com>
2021-07-02 22:52:40 +0200

    Add ability to stop the tests execution on the first fail.

--- helpers/src/gnl_assert.c
+++ helpers/src/gnl_assert.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <assert.h>
 #include "./gnl_colorshell.c"
 #include "../include/gnl_assert.h"
 
@@ -8,6 +10,10 @@ int gnl_assert(int (*fun)(), const char *fun_desc) {
         gnl_printf_green("PASSED");
     } else {
         gnl_printf_red("FAILED");
+
+#ifdef GNL_ASSERT_BLOCK
+        assert(res == 0);
+#endif
     }
 
     printf(" %s\n", fun_desc);
