dea3a6198566fd8f90de1605c4e6a075d9a720c7
Luca Agnello <luca@gnello.com>
2021-07-23 02:19:16 +0200

    Update interface

--- storage/include/gnl_storage.h
+++ storage/include/gnl_storage.h
@@ -11,4 +11,14 @@ extern struct gnl_storage *gnl_storage_init(int capacity, int limit, int replace
 
 extern void gnl_storage_destroy(struct gnl_storage *storage);
 
+extern int gnl_storage_open(struct gnl_storage *storage, char *filename, int flags);
+
+extern int gnl_storage_read(struct gnl_storage *storage, char *filename);
+
+extern int gnl_storage_write(struct gnl_storage *storage, char *filename, char *data);
+
+extern int gnl_storage_close(struct gnl_storage *storage, char *filename;
+
+extern int gnl_storage_remove(struct gnl_storage *storage, char *filename);
+
 #endif //GNL_FSS_STORAGE_H
