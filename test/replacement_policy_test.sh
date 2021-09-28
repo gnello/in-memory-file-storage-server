fa35a26866e78ce1a64e4ff5e6583912a8f3822d
Luca Agnello <luca@gnello.com>
2021-09-29 00:27:04 +0200

    Update socket name

--- test/replacement_policy_test.sh
+++ test/replacement_policy_test.sh
@@ -8,5 +8,5 @@ cd ../client/ || exit 1
 # run clients (-W)
 for COUNTER in 1 2 3 4 5 6 7 8
 do
-./main -f /tmp/LSOfilestorage_feature_test.sk -p -W $SCRIPTPATH/dataset/big/big$COUNTER.txt -D /tmp
+./main -f /tmp/LSOfilestorage_replacement_policy_test.sk -p -W $SCRIPTPATH/dataset/big/big$COUNTER.txt -D /tmp
 done
